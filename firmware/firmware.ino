8f52e43c244b28824cb7d3dda942f9505e1adc27
jochen@homeland <no-go@localhost>
2017-07-20 12:58:52 +0200

    still a bit buggy on questionmarks and other chars

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -11,10 +11,42 @@ const int rstPin = A0;
 const int rxPin  =  PB4;
 const int txPin  =  PB3;
 
-char readValue = '\0';
 int tick = 0;
+char buff[85] = {'\0'};
 
 SoftwareSerial mySerial(rxPin, txPin);
+
+// does it works?!?
+char umlReplace(char inChar) {
+  if (inChar <= -62) return ' ';
+  
+  if (inChar == -97) {
+    inChar = 224; // ß
+  } else if (inChar == -80) {
+    inChar = 248; // °
+  } else if (inChar == -67) {
+    inChar = 171; // 1/2
+  } else if (inChar == -78) {
+    inChar = 253; // ²
+  } else if (inChar == -92) {
+    inChar = 132; // ä
+  } else if (inChar == -74) {
+    inChar = 148; // ö
+  } else if (inChar == -68) {
+    inChar = 129; // ü
+  } else if (inChar == -124) {
+    inChar = 142; // Ä
+  } else if (inChar == -106) {
+    inChar = 153; // Ö
+  } else if (inChar == -100) {
+    inChar = 154; // Ü
+  } else if (inChar == -85) {
+    inChar = 0xAE; // <<
+  } else if (inChar == -69) {
+    inChar = 0xAF; // >>
+  }
+  return inChar;  
+}
 
 void setup() {
   pinMode(rxPin,     INPUT);
@@ -33,23 +65,31 @@ void setup() {
 }
 
 void loop() {
+  //while(mySerial.available()) {
   if (mySerial.available() > 0) {
-    readValue = mySerial.read();
+    char readValue = (char) mySerial.read();
+    buff[tick] = umlReplace(readValue);
+    buff[tick+1] = '\0';
+    
     if (readValue == '\n') {
       tick = 0;
       ssd1306_clear();
-      ssd1306_setpos(0, 0);
+      _delay_ms(200);
+      buff[0] = '\0';
+    } else {
+      tick++;
     }
-    ssd1306_string_font6x8(readValue);
-    tick++;
+    
     if (tick > 84) {
-      tick = 0;
+      tick = 1;
       ssd1306_clear();
-      ssd1306_setpos(0, 0);
-      ssd1306_string_font6x8(readValue);
+      _delay_ms(200);
+      buff[0] = umlReplace(readValue);
+      buff[1] = '\0';
     }
   }
-  delay(100);
+  ssd1306_setpos(0, 0);
+  ssd1306_string_font6x8(buff);
 }
 
 
